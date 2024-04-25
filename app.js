6ef11074e5c4cd980fa33458ab141c8403108b29
Nicolas Neira <111617452+NikoCodeN@users.noreply.github.com>
2024-04-24 19:27:47 -0500

    Add files via upload
    It is a Chronometer so easy for practice the DOM with JavaScript

---
+++ app.js
@@ -0,0 +1,64 @@
+const cronometro = document.getElementById('cronometro')
+const botonInicioPausa = document.getElementById('boton-inicio-pausa');
+const botonRiniciar = document.getElementById('boton-reiniciar');
+
+let [horas, minutos, segundos] = [0,0,0];
+
+let intervaloTiempo;
+let estadoCronometro = 'pausado';
+
+function actualizarCronometro (){
+    segundos++;
+    if(segundos/60 ===1){
+        segundos = 0;
+        minutos++;
+        
+        if (minutos/60 ===1){
+            minutos =0;
+            horas++;
+        }
+    }
+
+    const segundosConFormato = asignarFormato(segundos);
+    const minutosConFormato = asignarFormato(minutos);
+    const horasConFormato = asignarFormato(horas);
+
+    cronometro.innerText = `${horasConFormato}:${minutosConFormato}:${segundosConFormato}`
+
+}
+
+function  asignarFormato (unidadDeTiempo){
+    return unidadDeTiempo < 10 ? '0'+ unidadDeTiempo : unidadDeTiempo;
+}
+
+botonInicioPausa.addEventListener('click', function(){
+    if(estadoCronometro ==='pausado'){
+        intervaloTiempo = window.setInterval(actualizarCronometro, 1000);
+        botonInicioPausa.innerHTML = '<i class="bi bi-pause-fill"></i>';
+        botonInicioPausa.classList.remove('iniciar');
+        botonInicioPausa.classList.add('pausar');
+        estadoCronometro = 'andando';
+    }else{
+        window.clearInterval(intervaloTiempo);
+        botonInicioPausa.innerHTML='<i class="bi bi-play-fill"></i>';
+        botonInicioPausa.classList.remove('pausar');
+        botonInicioPausa.classList.add('iniciar')
+        estadoCronometro='pausado';
+    }
+})
+
+botonRiniciar.addEventListener('click',function(){
+    window.clearInterval(intervaloTiempo);
+
+    horas =0;
+    minutos = 0;
+    segundos = 0;
+
+    cronometro.innerText = '00:00:00';
+
+    botonInicioPausa.innerHTML='<i class="bi bi-play-fill"></i>'
+    botonInicioPausa.classList.remove('pausar');
+    botonInicioPausa.classList.add('iniciar');
+
+    estadoCronometro='pausado';
+})
